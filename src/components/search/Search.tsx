973a9d14df25ddf6009ad81c7454eb32e0571581
ngaridennis33 <ngaridennis33@gmail.com>
2024-02-07 14:28:25 +0300

    Began working on the sticky navbar

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -6,7 +6,7 @@ const Search = () => {
                 <svg width="25" height="25" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path fill="currentColor" d="M21.71 20.29L18 16.61A9 9 0 1 0 16.61 18l3.68 3.68a1 1 0 0 0 1.42 0a1 1 0 0 0 0-1.39ZM11 18a7 7 0 1 1 7-7a7 7 0 0 1-7 7Z"/>
                 </svg>
-                <input className={styles.input} placeholder="Search" required type="text"/>
+                <input className={styles.input} placeholder="Search..." required type="text" autoFocus/>
                 <span className={styles.inputBorder}></span>
     </form>
   )
