0fb014ffacbdc5da6a3efcc1ac83df103fa76acf
ngaridennis33 <ngaridennis33@gmail.com>
2024-03-13 17:50:15 +0300

    Updated the categories section

--- src/components/catGallery/CatGallery.tsx
+++ src/components/catGallery/CatGallery.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import styles from "./catGallery.module.scss";
 import Image from 'next/image';
 import Link from 'next/link';
@@ -6,28 +6,62 @@ import Link from 'next/link';
 const CatGallery = () => {
   return (
     <div className={styles.container}>
-        <div className={styles.col}>
-            <div className={styles.row}> <Image className={styles.img} src="/catkids.jpg" alt="" width={500} height={500}/><button className={styles.catBtn}><Link href="/"> Sale </Link></button></div>
-            <div className={styles.row}> <Image className={styles.img} src="/catkids.jpg" alt="" width={500} height={500}/><button className={styles.catBtn}><Link href="/"> Sale </Link></button></div>
-        </div>
-        <div className={styles.col}>
-        <div className={styles.row}> <Image className={styles.img} src="/catwomen.jpg" alt="" width={500} height={500}/><button className={styles.catBtn}><Link href="/"> Sale </Link></button></div>
-        </div>
-        <div className={`${styles.col} ${styles.col_l}`}>
+      <div className={styles.col}>
         <div className={styles.row}>
-        <div className={styles.col}>
-        <div className={styles.row}> <Image className={styles.img} src="/catmen.jpg" alt="" width={500} height={500}/><button className={styles.catBtn}><Link href="/"> Sale </Link></button></div>
+          <div className={styles.imgContainer}>
+            <Image className={styles.img} src="/catkids.jpg" alt="" width={500} height={500}/>
+            <div className={styles.overlay}></div>
+            <button className={styles.catBtn}><Link href="/"> Sale </Link></button>
+          </div>
         </div>
-        <div className={styles.col}>
-        <div className={styles.row}> <Image className={styles.img} src="/catbag.jpg" alt="" width={500} height={500}/><button className={styles.catBtn}><Link href="/"> Sale </Link></button></div>
-         </div>
-
+        <div className={styles.row}>
+          <div className={styles.imgContainer}>
+            <Image className={styles.img} src="/catkids.jpg" alt="" width={500} height={500}/>
+            <div className={styles.overlay}></div>
+            <button className={styles.catBtn}><Link href="/"> Sale </Link></button>
+          </div>
         </div>
-        <div className={styles.row}> <Image className={styles.img} src="/banner.jpg" alt="" width={500} height={500}/><button className={styles.catBtn}><Link href="/"> Sale </Link></button></div>
-            
+      </div>
+      <div className={styles.col}>
+        <div className={styles.row}>
+          <div className={styles.imgContainer}>
+            <Image className={styles.img} src="/catwomen.jpg" alt="" width={500} height={500}/>
+            <div className={styles.overlay}></div>
+            <button className={styles.catBtn}><Link href="/"> Sale </Link></button>
+          </div>
         </div>
+      </div>
+      <div className={`${styles.col} ${styles.col_l}`}>
+        <div className={`${styles.row} ${styles.row_l}`}>
+          <div className={styles.col}>
+            <div className={styles.row}>
+              <div className={styles.imgContainer}>
+                <Image className={styles.img} src="/catmen.jpg" alt="" width={500} height={500}/>
+                <div className={styles.overlay}></div>
+                <button className={styles.catBtn}><Link href="/"> Sale </Link></button>
+              </div>
+            </div>
+          </div>
+          <div className={styles.col}>
+            <div className={styles.row}>
+              <div className={styles.imgContainer}>
+                <Image className={styles.img} src="/catbag.jpg" alt="" width={500} height={500}/>
+                <div className={styles.overlay}></div>
+                <button className={styles.catBtn}><Link href="/"> Sale </Link></button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.row}>
+          <div className={styles.imgContainer}>
+            <Image className={styles.img} src="/banner.jpg" alt="" width={500} height={500}/>
+            <div className={styles.overlay}></div>
+            <button className={styles.catBtn}><Link href="/"> Sale </Link></button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CatGallery
+export default CatGallery;
