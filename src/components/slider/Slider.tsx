c551a96762898f2f47743a3bb8ebd7414a362e72
ngaridennis33 <ngaridennis33@gmail.com>
2024-03-14 03:51:29 +0300

    swipe effect update

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -8,13 +8,14 @@ import { MobileSwipper } from '../mobile-sipper/MobileSwiper'
 
 const Slider = () => {
     const [currSlide, setcurrSlide] = useState(0);
-    const numSlides = CategoriesData.length -1;
+    const numSlides = CategoriesData.length - 1;
 
-    const prevSlide=()=>{
-        setcurrSlide(currSlide === 0 ? numSlides : (prev) => prev -1 )
+    const prevSlide = () => {
+        setcurrSlide(currSlide === 0 ? currSlide : currSlide - 1);
     }
-    const nextSlide=()=>{
-        setcurrSlide(currSlide === numSlides ? 0 : (prev) => prev + 1 )
+
+    const nextSlide = () => {
+        setcurrSlide(currSlide === numSlides ? currSlide : currSlide + 1);
     }
 
     const handleClick = (direction: 'left' | 'right') => {
@@ -58,11 +59,11 @@ const Slider = () => {
 
         <div className={styles.iconWrapper}>
             
-        <div className={styles.icon} onClick={prevSlide}>
+        <div className={styles.icon} onClick={() => handleClick('left')}>
         <svg width={40} height={40} viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><title/><g data-name="Layer 2" id="Layer_2"><path fill='#000' d="M10.1,23a1,1,0,0,0,0-1.41L5.5,17H29.05a1,1,0,0,0,0-2H5.53l4.57-4.57A1,1,0,0,0,8.68,9L2.32,15.37a.9.9,0,0,0,0,1.27L8.68,23A1,1,0,0,0,10.1,23Z"/></g></svg>
         </div>
 
-        <div className={styles.icon} onClick={nextSlide}>
+        <div className={styles.icon} onClick={() => handleClick('right')}>
         <svg width={40} height={40} viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><title/><g data-name="Layer 2" id="Layer_2"><path fill='#000' d="M22,9a1,1,0,0,0,0,1.42l4.6,4.6H3.06a1,1,0,1,0,0,2H26.58L22,21.59A1,1,0,0,0,22,23a1,1,0,0,0,1.41,0l6.36-6.36a.88.88,0,0,0,0-1.27L23.42,9A1,1,0,0,0,22,9Z"/></g></svg>
         </div>
         </div>
